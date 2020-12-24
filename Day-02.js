8fdeb7a3c3b36316e31d7918f668a6301d9a0fd3
brigdan <kacperdev@gmail.com>
2020-12-24 06:02:41 +0000

    worked out more logic for the 2nd assignment

--- Day-02.js
+++ Day-02.js
@@ -6,14 +6,17 @@ var text = fs.readFileSync('Assets/Day-02/input.txt', 'utf8');
 
 // Match all numbers by Regular Expression
 var nums = text.match(/1?[0-9]|[1-9][0-9]/g);
+var passwordRegex = text.match(/\w:/g);
 
-// Declare array to store range
+// Declare array to store range, password letter and password text.
 const numRange = [];
-
+const passwordList =[];
 //convert 1d to 2d array and convert to number using map
 while(nums.length) numRange.push(nums.splice(0,2).map(x=>+x));
+while(passwordRegex.length) passwordList.push(passwordRegex);
 
-console.log(numRange);
+
+console.log(passwordList);
 
 
 /* TODO:  
